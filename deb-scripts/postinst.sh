ca2e209c9571b0d610ec255af37988383ac28f2c
Jan-Piet Mens <jp@mens.de>
2022-05-27 11:07:33 +0200

    Please!
    :-)

--- deb-scripts/postinst.sh
+++ deb-scripts/postinst.sh
@@ -1,5 +1,5 @@
 if [ -z "`id -u unbound 2> /dev/null`" ]; then
-adduser --system  --home /var/lib/unbound --gecos "dnsmasq" \
+adduser --system  --home /var/lib/unbound --gecos "Unbound" \
         --no-create-home --disabled-password \
         --quiet unbound || true
 fi
