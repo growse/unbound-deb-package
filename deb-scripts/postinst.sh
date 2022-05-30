22daa96bbafa85f9134882d0d710e262753810dd
Andrew Rowson <github@growse.com>
2022-05-30 11:47:48 +0100

    Build with libevent, create certs and set ownership on install

--- deb-scripts/postinst.sh
+++ deb-scripts/postinst.sh
@@ -1,5 +1,8 @@
 if [ -z "`id -u unbound 2> /dev/null`" ]; then
-adduser --system  --home /var/lib/unbound --gecos "Unbound" \
+/usr/sbin/adduser --system  --home /var/lib/unbound --gecos "Unbound" \
         --no-create-home --disabled-password \
         --quiet unbound || true
+
+/usr/sbin/unbound-control-setup
+/usr/bin/chown unbound unbound_control.pem unbound_control.key unbound_server.pem unbound_server.key
 fi
